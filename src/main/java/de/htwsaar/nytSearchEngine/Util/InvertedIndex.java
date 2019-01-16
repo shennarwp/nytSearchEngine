622c98a3772129d4de382e4351d98f6d3ded7029
TG <orggottschalk@web.de>
2019-01-16 17:07:27 +0100

    Methodenrümpfe erstellt

---
+++ src/main/java/de/htwsaar/nytSearchEngine/Util/InvertedIndex.java
@@ -0,0 +1,28 @@
+package de.htwsaar.nytSearchEngine.Util;
+
+import de.htwsaar.nytSearchEngine.model.Posting;
+
+import java.util.List;
+
+public class InvertedIndex {
+
+   public List<Posting> getIndexList(String term){
+
+        return null;
+    }
+
+   public int getDF(String term){
+
+        return 0;
+    }
+
+    public int getSize(){
+
+       return 0;
+    }
+
+    int getLength(long did){
+
+       return 0;
+    }
+}
